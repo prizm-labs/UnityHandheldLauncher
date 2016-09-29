890174b3ba5e04e564230d339516fc07ff2535cc
Michael Garrido <michael.a.garrido@gmail.com>
2016-09-29 12:58:42 -0700

    web socket message handling refactored

--- Handheld Launcher/Assets/Scripts/Networking/WebsocketClient.cs
+++ Handheld Launcher/Assets/Scripts/Networking/WebsocketClient.cs
@@ -5,6 +5,7 @@ using WebSocketSharp;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
+
 public class WebsocketClient : MonoBehaviour {
 	public static WebsocketClient instance;
 
@@ -15,7 +16,13 @@ public class WebsocketClient : MonoBehaviour {
 
 	public string ip;
 	public string sessionName;
-	public string websocketService = "players";
+
+	static string websocketService = "players";
+	static int websocketPort = 4649;
+
+
+	public WebsocketMessageQueue messageQueue;
+
 
 	private bool debugging = true;	//displays the websocket message in text 
 
@@ -23,6 +30,9 @@ public class WebsocketClient : MonoBehaviour {
 
 	public WebSocket ws;
 	public string wsMessage = "";
+
+
+
 	void Awake(){
 		if (instance == null) {
 			instance = this;
@@ -30,54 +40,70 @@ public class WebsocketClient : MonoBehaviour {
 			Destroy (gameObject);
 	}
 
+	void Start() {
+
+		gameObject.AddComponent<WebsocketMessageQueue> ();
+		messageQueue = gameObject.GetComponent<WebsocketMessageQueue> ();
+
+		messageQueue.AddHandler (Topics.PlayerDescriptor, OnPlayerDescriptor);
+	}
+
+	void OnPlayerDescriptor(JSONObject data) {
+		Debug.Log ("Player descriptor");
+	}
+
 
 	void Update() {
-		if (messageLog.Count > 0) {	//if we have messages in our backlog, we have to process them in the main thread
-			int numMessages = messageLog.Count;
 
-			for (int i = 0; i < numMessages; i++) {
-				wsMessage = messageLog [i];
-				if (debugging) {
-					GameObject.Find ("Canvas/raw").GetComponent<Text> ().text = "raw ws message: '" + wsMessage + "';";
-					try {
-						JSONObject jsonText = new JSONObject (wsMessage);
-						GameObject.Find ("Canvas/json").GetComponent<Text> ().text = "message in json: '" + jsonText.ToString () + "';";
-					} catch (System.Exception e) {
-						Debug.LogWarning ("probably improperly formatted json in debugging");
-						Debug.LogWarning (e);
-					}
 
-				}
-
-				JSONObject d = new JSONObject (wsMessage);
-				//Debug.Log ("received object from json!\n" + d.ToString ());
-				Debug.Log ("receieved packet type: " + d.GetField ("type").str);
-				switch (d.GetField ("type").str) {
-				case("PlayerDescriptor"):
-					Debug.Log ("received a 'PlayerDescriptor'");
-					PlayerDescriptorData (d);
-					break;
-				case("ZoneDescriptor"):
-					Debug.Log ("got into a 'ZoneDescriptor'");
-					ZoneDescriptorData (d);
-					break;
-				case("PieceDescriptor"):
-					PieceDescriptorData (d);
-					break;
-				default:
-					break;
-
-				}
-			}
-
-			messageLog.RemoveRange (0, numMessages);
-		}
+//		if (messageLog.Count > 0) {	//if we have messages in our backlog, we have to process them in the main thread
+//			int numMessages = messageLog.Count;
+//
+//			for (int i = 0; i < numMessages; i++) {
+//				wsMessage = messageLog [i];
+//				if (debugging) {
+//					GameObject.Find ("Canvas/raw").GetComponent<Text> ().text = "raw ws message: '" + wsMessage + "';";
+//					try {
+//						JSONObject jsonText = new JSONObject (wsMessage);
+//						GameObject.Find ("Canvas/json").GetComponent<Text> ().text = "message in json: '" + jsonText.ToString () + "';";
+//					} catch (System.Exception e) {
+//						Debug.LogWarning ("probably improperly formatted json in debugging");
+//						Debug.LogWarning (e);
+//					}
+//
+//				}
+//
+//				JSONObject d = new JSONObject (wsMessage);
+//				//Debug.Log ("received object from json!\n" + d.ToString ());
+//				Debug.Log ("receieved packet type: " + d.GetField ("type").str);
+//				switch (d.GetField ("type").str) {
+//				case("PlayerDescriptor"):
+//					Debug.Log ("received a 'PlayerDescriptor'");
+//					PlayerDescriptorData (d);
+//					break;
+//				case("ZoneDescriptor"):
+//					Debug.Log ("got into a 'ZoneDescriptor'");
+//					ZoneDescriptorData (d);
+//					break;
+//				case("PieceDescriptor"):
+//					PieceDescriptorData (d);
+//					break;
+//				default:
+//					break;
+//
+//				}
+//			}
+//
+//			messageLog.RemoveRange (0, numMessages);
+//		}
 	}
 
 	public void BeginConnection(string ipAddress, string name){
-		ip = ipAddress;
-		sessionName = name;
-		ws = new WebSocket("ws://"+ip+":4649/"+websocketService);
+
+		string wsUrl = String.Format("ws://{0}:{1}/{2}", ipAddress, websocketPort,websocketService );
+		Debug.Log ("Connecting to Webscoket server @ " + wsUrl);
+
+		ws = new WebSocket(wsUrl);
 
 		//var ver = Application.unityVersion;
 		ws.OnOpen += (sender, e) =>
@@ -86,9 +112,11 @@ public class WebsocketClient : MonoBehaviour {
 
 		ws.OnMessage += (sender, e) =>
 		{
-			//wsMessage = e.Data;
+			wsMessage = e.Data;
 			Debug.Log("got a message: " + wsMessage);
-			messageLog.Add(e.Data);
+
+			messageQueue.QueueMessage(wsMessage);
+			//messageLog.Add(e.Data);
 		};
 
 		ws.OnError += (sender, e) =>
@@ -99,7 +127,7 @@ public class WebsocketClient : MonoBehaviour {
 
 		ws.OnClose += (sender, e) => {
 			Debug.LogError("Closing");
-			ws.Send(String.Format("Player disconnected"));
+			//ws.Send(String.Format("Player disconnected"));
 
 		};
 		
